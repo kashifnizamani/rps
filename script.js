0a4a3183c220c5ed36fce6259ee47b93cb359bf8
Kashif <nizamanikashif2003@gmail.com>
2024-10-03 23:06:22 +0500

    coded the getCopmuterChoice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+let choice = Math.floor(Math.random() * 3) + 1;
+
+function getComputerChoice(){
+
+    switch(choice){
+  case 1:
+     return "Rock";
+
+  break;
+  case 2:
+    return "Paper";
+  break;
+  case 3:
+    return "Scissors";
+  break;
+
+    }
+
+}
+console.log(getComputerChoice())
+
